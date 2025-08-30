08279690c6a21dd4a2ae2fbf0062eee2a6b0c55e
Carlos <karurosualexandresouza1234@gmail.com>
2025-08-29 21:58:32 -0400

    Issue servidor resolvido

--- dad2025-s2-rmi-servidor-cidade/src/main/java/rmi/ICidadeManager.java
+++ dad2025-s2-rmi-servidor-cidade/src/main/java/rmi/ICidadeManager.java
@@ -3,8 +3,9 @@ package rmi;
 import model.Cidade;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface ICidadeManager extends Remote {
-    public List<Cidade> getCidades();
+    public List<Cidade> getCidades() throws RemoteException;
 }
